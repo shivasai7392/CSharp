2a94120b64c12f1165edb293284b6d2c9c2be457
Shivasaikrishna Thota <shivasai7392@outlook.com>
2020-11-23 10:59:28 +0530

    Reading & writing to console

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to C# Training");
+            Console.WriteLine("Please enter your name");
+            string username = Console.ReadLine();
+
+            //using concatenation
+            Console.WriteLine("Hello " + username);
+
+            //using placeholder
+            Console.WriteLine("Hello {0}", username);
+
+
         }
     }
 }
